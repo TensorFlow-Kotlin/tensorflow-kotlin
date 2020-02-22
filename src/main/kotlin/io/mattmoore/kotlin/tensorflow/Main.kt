ec483b7e54a8f1d682f422165516acc0a4943d7e
Matt Moore <matt@mattmoore.io>
2020-02-22 13:05:40 -0600

    Move graph example from main to TensorFlow.

--- src/main/kotlin/io/mattmoore/kotlin/tensorflow/Main.kt
+++ src/main/kotlin/io/mattmoore/kotlin/tensorflow/Main.kt
@@ -1,17 +1,5 @@
 package io.mattmoore.kotlin.tensorflow
 
-import org.tensorflow.Graph
-import org.tensorflow.Session
-import org.tensorflow.Tensor
-import org.tensorflow.TensorFlow
-
 fun main(args: Array<String>) {
-    Graph().use { g ->
-        val value = "Hello from " + TensorFlow.version()
-        Tensor.create(value.toByteArray(charset("UTF-8"))).use { t ->
-            // The Java API doesn't yet include convenience functions for adding operations.
-            g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build()
-        }
-        Session(g).use { s -> s.runner().fetch("MyConst").run()[0].use { output -> println(String(output.bytesValue())) } }
-    }
+    graphExample()
 }
---
+++ src/main/kotlin/io/mattmoore/kotlin/tensorflow/TensorFlow.kt
@@ -0,0 +1,17 @@
+package io.mattmoore.kotlin.tensorflow
+
+import org.tensorflow.Graph
+import org.tensorflow.Session
+import org.tensorflow.Tensor
+import org.tensorflow.TensorFlow
+
+fun graphExample() {
+    Graph().use { g ->
+        val value = "Hello from " + TensorFlow.version()
+        Tensor.create(value.toByteArray(charset("UTF-8"))).use { t ->
+            // The Java API doesn't yet include convenience functions for adding operations.
+            g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build()
+        }
+        Session(g).use { s -> s.runner().fetch("MyConst").run()[0].use { output -> println(String(output.bytesValue())) } }
+    }
+}
