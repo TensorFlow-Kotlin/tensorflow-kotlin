84b1e06b3ab526b79145a61318c0d9a973611530
Matt Moore <matt@mattmoore.io>
2020-09-29 07:19:19 -0500

    Fix build

--- src/main/kotlin/dev/tfk/Main.kt
+++ src/main/kotlin/dev/tfk/Main.kt
@@ -4,6 +4,7 @@ fun main(args: Array<String>) {
   val graph = Graph.fromFile("models/simple.pb")
   val inputData = arrayOf(arrayOf(4f, 3f, 2f, 1f))
   val prediction = graph.predict(inputData)
+  println("TensorFlow version: ${TensorFlow.version()}")
+  println("Inputs: ${inputData.first().toList()}")
   println("Prediction: ${prediction?.toList()}")
-  println(TensorFlow.version())
 }
